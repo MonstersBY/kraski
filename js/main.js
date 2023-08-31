52843d9d40e2342c47a0fcfd4b189c9eac5c5c55
MonstersBY <monsters.alex0@gmail.com>
2023-08-31 17:26:22 +0300

    feat: add brand, selection-by-parameters

--- js/main.js
+++ js/main.js
@@ -13,4 +13,60 @@ $('.switcher__btn').on('click', function(evt){
     $($switcher_contents[index]).addClass('active');
 })
 
+if($('.selection-bp').length) {
+    $('.selection-bp-form--first').on( "submit", function(e) {
+        e.preventDefault();
+        $(this).css('display', 'none')
+        $('.selection-bp-form--second').css('display', 'flex')
+        $('.selection-bp-form--second').css('opacity', '1')
+        $('.selection-bp__progress-step')[0].classList.add("active")
+        $('.selection-bp__progress-line-pink').addClass('second')
+    });
+    $('.selection-bp-form--second').on( "submit", function(e) {
+        e.preventDefault();
+        $(this).css('display', 'none')
+        $('.selection-bp-form--third').css('display', 'flex')
+        $('.selection-bp-form--third').css('opacity', '1')
+        $('.selection-bp__progress-step')[1].classList.add("active")
+        $('.selection-bp__progress-line-pink').addClass('third')
+    });
+
+    if (screen.width < 769) {
+        // var fixedBlock = $('#cart_fixed-block')
+
+        $('.selection-bp-form-container').each(function(){
+            const orininalPosition = $(this)
+            const disabled = $(this).find('.selection-bp-form-container-box')
+            const cartPosition = $(this).find('.btn-color')
+            console.log(cartPosition);
+            $(window).scroll(function () {
+                var scrollPosition = $(window).scrollTop()
+                var cartOffset = cartPosition.offset().top + cartPosition.height() - $(window).height()
+                var orininalOffset = orininalPosition.offset().top + orininalPosition.height() - $(window).height()
+                if(cartOffset>0) {
+
+
+                    console.log(scrollPosition);
+                    console.log(cartOffset);
+                    console.log(orininalOffset);
+                    
+                    if (scrollPosition >= orininalOffset) {
+                        disabled.addClass('disable')
+                    } else {
+                        disabled.removeClass('disable')
+                    }
+                }
+    
+                // if (scrollPosition >= cartOffset) {
+                //     fixedBlock.hide()
+                // } else {
+                //     fixedBlock.show()
+                // }
+            })
+        })
+
+
+    }
+}
+
 })
