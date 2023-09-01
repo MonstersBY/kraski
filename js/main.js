09da24ced2fbcf5a9415c2445b8549ac222171ff
MonstersBY <monsters.alex0@gmail.com>
2023-09-01 18:53:06 +0300

    feat: add catalog desk

--- js/main.js
+++ js/main.js
@@ -32,41 +32,171 @@ if($('.selection-bp').length) {
     });
 
     if (screen.width < 769) {
-        // var fixedBlock = $('#cart_fixed-block')
-
         $('.selection-bp-form-container').each(function(){
             const orininalPosition = $(this)
             const disabled = $(this).find('.selection-bp-form-container-box')
             const cartPosition = $(this).find('.btn-color')
-            console.log(cartPosition);
             $(window).scroll(function () {
                 var scrollPosition = $(window).scrollTop()
                 var cartOffset = cartPosition.offset().top + cartPosition.height() - $(window).height()
                 var orininalOffset = orininalPosition.offset().top + orininalPosition.height() - $(window).height()
                 if(cartOffset>0) {
-
-
-                    console.log(scrollPosition);
-                    console.log(cartOffset);
-                    console.log(orininalOffset);
-                    
                     if (scrollPosition >= orininalOffset) {
                         disabled.addClass('disable')
                     } else {
                         disabled.removeClass('disable')
                     }
                 }
-    
-                // if (scrollPosition >= cartOffset) {
-                //     fixedBlock.hide()
-                // } else {
-                //     fixedBlock.show()
-                // }
             })
         })
-
-
     }
+}
+
+if($('.catalog__sidebar').length) {
+    //Показать еще
+    $('.catalog__sidebar-form-item--show').on('click', function(){
+        $(this).siblings('.hidden').removeClass('hidden')
+        $(this).css('display', 'none')
+        console.log($(this).siblings('.hidden'));
+    })
+
+    // ЦЕНА
+    var costRange = document.getElementById('cost-range');
+    const arrCost = {
+        min: 0,
+        max: 10000,
+    }
+    noUiSlider.create(costRange, {
+        start: [arrCost.min, arrCost.max],
+        connect: true,
+        step: 1,
+        range: {
+            'min': [arrCost.min],
+            'max': [arrCost.max]
+        }
+    });
+
+    var costRangeFrom = document.getElementById('cost-range-from');
+    var costRangeTo = document.getElementById('cost-range-to');
+
+    costRange.noUiSlider.on('update', function (values, handle) {
+
+        var value = values[handle].slice(0, -3);
+
+        if (handle) {
+            if (arrCost.max == Number(value)) {
+                costRangeTo.value = '';
+            } else {
+                value = value.split('').join('').replace(/\B(?=(\d{3})+(?!\d))/g, " ").split('').join('').trim();
+                costRangeTo.value = value;
+            }
+        } else {
+            if (arrCost.min == Number(value)) {
+                costRangeFrom.value = '';
+            } else {
+                value = value.split('').join('').replace(/\B(?=(\d{3})+(?!\d))/g, " ").split('').join('').trim();
+                costRangeFrom.value = value;
+            }
+        }
+    });
+
+    costRangeFrom.addEventListener('keydown', function (e) {
+        if ((e.key.length === 1 && /^[^\d\s]+$/.test(e.key))) {
+            e.preventDefault();
+            return false;
+        }
+    });
+    costRangeFrom.addEventListener('keyup', function (e) {
+        let value = this.value.replace(/\s+/g, '');
+        costRange.noUiSlider.set([value, null]);
+    });
+
+    costRangeTo.addEventListener('keydown', function (e) {
+        if ((e.key.length === 1 && /^[^\d\s]+$/.test(e.key))) {
+            e.preventDefault();
+            return false;
+        }
+    });
+    costRangeTo.addEventListener('keyup', function (e) {
+        let value = this.value.replace(/\s+/g, '');
+        costRange.noUiSlider.set([null, value]);
+    });
+
+    //Объем
+    var volumeRange = document.getElementById('volume-range');
+    const arrVolume = {
+        min: 0,
+        max: 10000,
+    }
+    noUiSlider.create(volumeRange, {
+        start: [arrVolume.min, arrVolume.max],
+        connect: true,
+        step: 1,
+        range: {
+            'min': [arrVolume.min],
+            'max': [arrVolume.max]
+        }
+    });
+
+    var volumeRangeFrom = document.getElementById('volume-range-from');
+    var volumeRangeTo = document.getElementById('volume-range-to');
+
+    volumeRange.noUiSlider.on('update', function (values, handle) {
+        var value = values[handle].slice(0, -3);
+
+        if (handle) {
+            if (arrVolume.max == Number(value)) {
+                volumeRangeTo.value = '';
+            } else {
+                value = value.split('').join('').replace(/\B(?=(\d{3})+(?!\d))/g, " ").split('').join('').trim();
+                volumeRangeTo.value = value;
+            }
+        } else {
+            if (arrVolume.min == Number(value)) {
+                volumeRangeFrom.value = '';
+            } else {
+                value = value.split('').join('').replace(/\B(?=(\d{3})+(?!\d))/g, " ").split('').join('').trim();
+                volumeRangeFrom.value = value;
+            }
+        }
+    });
+
+    volumeRangeFrom.addEventListener('keydown', function (e) {
+        if ((e.key.length === 1 && /^[^\d\s]+$/.test(e.key))) {
+            e.preventDefault();
+            return false;
+        }
+    });
+    volumeRangeFrom.addEventListener('keyup', function (e) {
+        let value = this.value.replace(/\s+/g, '');
+        volumeRange.noUiSlider.set([value, null]);
+    });
+
+    volumeRangeTo.addEventListener('keydown', function (e) {
+        if ((e.key.length === 1 && /^[^\d\s]+$/.test(e.key))) {
+            e.preventDefault();
+            return false;
+        }
+    });
+    volumeRangeTo.addEventListener('keyup', function (e) {
+        let value = this.value.replace(/\s+/g, '');
+        volumeRange.noUiSlider.set([null, value]);
+    });
+
+}
+if($('.catalog__sort').length) {
+    $('.catalog__sort-container--text').on('click', function(){
+        $(this).parents('.catalog__sort-container').addClass('active')
+    })
+    $(document).mouseup(function (e) {
+        var container = $('.catalog__sort-container');
+        if (container.has(e.target).length === 0){
+            container.removeClass('active')
+        }
+    });
+    $('.catalog__sort-container-item-radio').on('click', function(){
+        $('.catalog__sort-container--text').text($(this).find('p').text())
+    })
 }
 
 })
---
+++ js/swiper.js
@@ -0,0 +1,121 @@
+$(document).ready(function() {
+    function remToPx(remValue) {
+        // Получаем текущий базовый размер шрифта (font-size) из элемента <html>
+        var htmlFontSize = parseFloat(getComputedStyle(document.documentElement).fontSize);
+    
+        // Переводим значение из rem в px
+        var pxValue = remValue * htmlFontSize;
+    
+        // Округляем значение до целых пикселей (по желанию)
+        return Math.round(pxValue) + 'px';
+    }
+    let catalogSmallSwiper = new Swiper('.catalog-small-swiper', {
+        slidesPerView: 'auto',
+        spaceBetween: `${remToPx(2)}rem`,
+        navigation: {
+            prevEl: '.catalog-small-prev-btn',
+            nextEl: '.catalog-small-next-btn',
+          },
+    });
+    let catalogBrandSwiper = new Swiper('.catalog__brand-swiper', {
+        slidesPerView: 'auto',
+        spaceBetween: `${remToPx(1)}rem`,
+        navigation: {
+            prevEl: '.catalog__brand-prev-btn',
+            nextEl: '.catalog__brand-next-btn',
+          },
+    });
+
+    let recomendationSlider = new Swiper('.recomendation__slider', {
+        slidesPerView: 4,
+        spaceBetween: `${remToPx(2)}rem`,
+        navigation: {
+          prevEl: '.recomendation__slider-prev',
+          nextEl: '.recomendation__slider-next',
+        },
+        breakpoints: {
+          320: {
+            slidesPerView: 2,
+            spaceBetween: `${remToPx(2)}rem`,
+            grid: {
+              rows: 2,
+              fill: 'rows',
+            },
+            pagination: {
+              el: '.popular-pagination--mob',
+              clickable: true,
+            },
+          },
+          // Медиа-запрос для ширины больше 768px
+          769: {
+            slidesPerView: 4,
+            grid: {
+              rows: 1,
+              fill: 'columns',
+            },
+          },
+        },
+    });
+
+    let instrumentsSlider = new Swiper('.instruments__slider', {
+        slidesPerView: 4,
+        spaceBetween: `${remToPx(2)}rem`,
+        navigation: {
+          prevEl: '.instruments__slider-prev',
+          nextEl: '.instruments__slider-next',
+        },
+        breakpoints: {
+          320: {
+            slidesPerView: 2,
+            spaceBetween: `${remToPx(2)}rem`,
+            grid: {
+              rows: 2,
+              fill: 'rows',
+            },
+            pagination: {
+              el: '.popular-pagination--mob',
+              clickable: true,
+            },
+          },
+          // Медиа-запрос для ширины больше 768px
+          769: {
+            slidesPerView: 4,
+            grid: {
+              rows: 1,
+              fill: 'columns',
+            },
+          },
+        },
+    });
+
+    let preparatoryWorkSlider = new Swiper('.preparatory-work__slider', {
+        slidesPerView: 4,
+        spaceBetween: `${remToPx(2)}rem`,
+        navigation: {
+          prevEl: '.preparatory-work__slider-prev',
+          nextEl: '.preparatory-work__slider-next',
+        },
+        breakpoints: {
+          320: {
+            slidesPerView: 2,
+            spaceBetween: `${remToPx(2)}rem`,
+            grid: {
+              rows: 2,
+              fill: 'rows',
+            },
+            pagination: {
+              el: '.popular-pagination--mob',
+              clickable: true,
+            },
+          },
+          // Медиа-запрос для ширины больше 768px
+          769: {
+            slidesPerView: 4,
+            grid: {
+              rows: 1,
+              fill: 'columns',
+            },
+          },
+        },
+    });
+})
